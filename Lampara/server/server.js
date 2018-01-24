7d4ca8b44bb791611de6a7720035b7241c90b6bf
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-24 16:15:11 -0300

    se agrega comunicacion socket con controlador

--- Lampara/server/server.js
+++ Lampara/server/server.js
@@ -1,7 +1,11 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-  mqtt = require('mqtt');
+	io = require("socket.io").listen(server),
+  mqtt = require('mqtt'),
+	client = mqtt.connect({host:'192.168.1.166',port:1883});
+
+
 
 //Puerto donde corre el sistema
 server.listen(8080);
@@ -11,3 +15,10 @@ app.use(express.static(__dirname + '/assets'));
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/views/index.html');
 });
+
+io.sockets.on('connection', function(socket) {
+	socket.on('sendCom', function(data){
+		client.publish('luz',data.value);
+		console.log("se envio por mqtt, el dato: " + data.value);
+	})
+}
